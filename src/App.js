287b54f9253037a77f1fc7c4b84fdfccc3f1e279
wonseok-do <wsdo970@gmail.com>
2022-08-08 18:00:36 +0900

    redux 사용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useSelector, useDispatch } from "react-redux";
+import { changeName } from "./redux/modules/cat";
 
 function App() {
+  const cat = useSelector((state) => state.cat);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>name:::{cat.name}</p>
+      <button
+        onClick={() => {
+          dispatch(changeName("루비"));
+        }}
+      >
+        이름바꾸기
+      </button>
     </div>
   );
 }
